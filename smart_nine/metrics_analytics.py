2175b5b63dc3e2c6fca93db7d8bab0106ccd7fc1
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2020-12-30 12:58:07 -0800

    reformatted to CLI style

--- smart_nine/metrics_analytics.py
+++ smart_nine/metrics_analytics.py
@@ -1,3 +1,4 @@
+import constants
 from scipy.signal import find_peaks
 
 class MetricsAnalytics():
@@ -7,17 +8,20 @@ class MetricsAnalytics():
     def __init__(self):
         pass        
         
-    def find_top_nine_peaks(self, ts_list, like_list, filename_list):
+    def find_top_nine_peaks(self, ts_list, like_list, filename_list, username, year):
         """
         Returns subset of input lists based on top nine filtering
         """
-        coeff = int(len(like_list) / 12)
-        if coeff < 1:
-            coeff = 1
+        if len(like_list) < constants.MATRIX_SIZE:
+            raise ValueError(f"Instagram account: @{username} doesn't have at least 9 photographs in year: {year}")
 
-        peak_indices = (find_peaks(like_list, distance=coeff)[0])
+        distance = int(len(like_list) / constants.SPACING)
+        if distance < constants.MIN_SPACING:
+            distance = constants.MIN_SPACING
 
-        if len(peak_indices) < 9:
+        peak_indices = (find_peaks(like_list, distance=distance)[0])
+
+        if len(peak_indices) < constants.MATRIX_SIZE:
             print(f"Failed to find peaks, finding max points instead.")
             peak_indices = [i[0] for i in sorted(enumerate(like_list), key=lambda k: k[1], reverse=True)][0:9]
 
