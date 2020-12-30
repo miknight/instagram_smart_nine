d9d06c4c6be45168d6c32292ccc6879308392b49
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2020-12-30 01:55:41 -0800

    fixed timezone name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ config_dict = json.load(open("config.json"))
 better = bn.BetterNine(usernames=config_dict["usernames"],
                        scrapper_user=config_dict["scrapper_user"],
                        password=config_dict["password"],
-                       tz=config_dict["tz"],
+                       tz=config_dict["timezone"],
                        limit_date=config_dict["limit_date"])
 
 better.better_nine(scrape_flag=False)
